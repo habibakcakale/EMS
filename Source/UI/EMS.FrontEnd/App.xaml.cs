7645341c3a0787af453e7935019ae23bb2abc2df
Habib Akcakale <habibakcakale@hotmail.com>
2020-10-29 16:54:20 +0300

    Add project files.

---
+++ Source/UI/EMS.FrontEnd/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace EMS.FrontEnd {
+    using System.Windows;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyModel;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Serilog;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App {
+        private IHost host;
+
+        protected override async void OnStartup(StartupEventArgs e) {
+            host = CreateHostBuilder(e.Args).Build();
+            await host.StartAsync();
+            using var scope = host.Services.CreateScope();
+            scope.ServiceProvider.GetRequiredService<MainWindow>().Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e) {
+            await host.StopAsync();
+            host.Dispose();
+            base.OnExit(e);
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args = null) {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder =>
+                    builder.AddJsonFile("appsettings.json").AddEnvironmentVariables().Build())
+                .ConfigureServices(services => { services.AddScoped<MainWindow>(); })
+                .UseSerilog((context, configuration) =>
+                    configuration.ReadFrom.Configuration(context.Configuration, DependencyContext.Default));
+        }
+    }
+}
